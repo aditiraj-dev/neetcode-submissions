4d59b319d898a03b7941f8bbfaea93079939995d
Aditi Raj <aditiraj.novelist@gmail.com>
2026-05-28 17:30:17 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //topoligical sorting cannot exist in a graph with a cycle
+
+        vector<int> indegree(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+
+        for (auto& pre : prerequisites) {
+            indegree[pre[1]]++;
+            adj[pre[0]].push_back(pre[1]);
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int finish = 0;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            finish++;
+            for(int nei : adj[node])
+            {
+                indegree[nei]--;
+                if(indegree[nei] == 0) q.push(nei);
+            }
+        }
+
+        return finish == numCourses;
+    }
+};
