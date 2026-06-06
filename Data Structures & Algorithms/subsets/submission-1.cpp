22bdca97bdd2e936faefbabef2a974180ef16158
Aditi Raj <aditiraj.novelist@gmail.com>
2026-06-06 21:10:15 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<int>> res;
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subset;
+        backtrack(0, nums, subset);
+
+        return res;
+    }
+
+    void backtrack(int i, vector<int>& nums, vector<int>& subset)
+    {
+        if(i >= nums.size()) {
+            res.push_back(subset);
+            return;
+        }
+        subset.push_back(nums[i]);
+        backtrack(i+1, nums, subset);
+        subset.pop_back();
+        backtrack(i+1, nums, subset);
+
+    }
+};
