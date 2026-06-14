c8deeb0f541ac75514949d23d99b9ca1c39f9935
Aditi Raj <aditiraj.novelist@gmail.com>
2026-06-14 16:09:44 +0530

    Add: design-twitter-feed - submission-8

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-8.cpp
@@ -0,0 +1,58 @@
+class Twitter {
+public:
+    unordered_map<int, unordered_set<int>> followMap;
+    unordered_map<int, vector<vector<int>>> tweetMap;
+    int count;
+    Twitter() {
+        count = 0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweetMap[userId].push_back({count++, tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        auto compare = [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> minHeap(compare);
+        followMap[userId].insert(userId);
+        for(int followeeId : followMap[userId])
+        {
+            if(tweetMap.count(followeeId))
+            {
+                const vector<vector<int>>& tweets = tweetMap[followeeId];
+                int index = tweets.size() - 1;
+                minHeap.push({tweets[index][0], tweets[index][1], followeeId, index});
+            }
+        }
+
+        vector<int> res;
+
+        while(!minHeap.empty() && res.size() < 10)
+        {
+            vector<int> curr = minHeap.top();
+            minHeap.pop();
+            res.push_back(curr[1]);
+            int index = curr[3];
+            if(index > 0)
+            {
+                const vector<int>& tweet = tweetMap[curr[2]][index - 1]; //aka their now most recent tweet
+                minHeap.push({tweet[0], tweet[1], curr[2], index - 1});
+            }
+        }
+
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        followMap[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if(followMap[followerId].count(followeeId))
+        {
+            followMap[followerId].erase(followeeId);
+        }
+    }
+};
