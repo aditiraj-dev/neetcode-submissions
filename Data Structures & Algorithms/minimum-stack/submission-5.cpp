e5b860c7ebeddc863d0ea582e58de33b3a10af76
Aditi Raj <aditiraj.novelist@gmail.com>
2026-06-15 18:48:50 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s1.push(val);
+        if(!s2.empty() && s2.top() < val)
+        {
+            val = s2.top();
+        }
+        s2.push(val);
+    }
+    
+    void pop() {
+        s1.pop();
+        s2.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
