760b491d0ef4f80beab26d97a02428f747aae138
Aditi Raj <aditiraj.novelist@gmail.com>
2026-06-18 19:28:48 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    unordered_map<int, bool> memo;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        memo[s.length()] = true;
+        return helper(s, wordDict, 0);
+    }
+
+    bool helper(string s, vector<string>& wordDict, int i)
+    {
+        if(memo.count(i)) return memo[i];
+        
+        for(const string& w : wordDict)
+        {
+            if(i + w.length() <= s.size() && s.substr(i, w.length()) == w)
+            {
+                if(helper(s, wordDict, i + w.length())) 
+                {
+                    memo[i] = true;
+                    return true;
+                }
+            }
+        }
+
+        memo[i] = false;
+        return false;
+    }
+};
