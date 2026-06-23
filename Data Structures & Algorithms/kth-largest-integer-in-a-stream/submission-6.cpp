4f9eececf4943a42488a2fd2a6de5026e66ee9a6
Aditi Raj <aditiraj.novelist@gmail.com>
2026-06-23 20:44:37 +0530

    Add: kth-largest-integer-in-a-stream - submission-6

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-6.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+public:
+    int k = 0;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int n : nums)
+        {
+            minHeap.push(n);
+            if(minHeap.size() > k) minHeap.pop();
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if(minHeap.size() > k) minHeap.pop();
+        return minHeap.top();
+    }
+};
