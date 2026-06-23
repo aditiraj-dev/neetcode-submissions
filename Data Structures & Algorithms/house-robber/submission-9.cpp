94062efdbbabe5291fed16fe9f9d2cf4baac215d
Aditi Raj <aditiraj.novelist@gmail.com>
2026-06-24 00:15:22 +0530

    Add: house-robber - submission-9

---
+++ Data Structures & Algorithms/house-robber/submission-9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size() + 2);
+        dp[nums.size()] = 0;
+        dp[nums.size() + 1] = 0;
+
+        for(int i = nums.size() - 1; i>=0; i--)
+        {
+            dp[i] = max(nums[i] + dp[i+2], dp[i+1]);
+        }
+
+        return dp[0];
+    }
+};
