e1ab43d9512a3fdfe4e25cc095bcd0f1096ad084
Aditi Raj <aditiraj.novelist@gmail.com>
2026-08-12 11:36:14 +0530

    Add: house-robber - submission-17

---
+++ Data Structures & Algorithms/house-robber/submission-17.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> memo;
+    int rob(vector<int>& nums) {
+        memo.resize(nums.size(), -1);
+        return helper(nums, 0);
+    }
+
+    int helper(vector<int>& nums, int i)
+    {
+        if(i >= nums.size()) return 0;
+        if(memo[i] != -1) return memo[i];
+
+        memo[i] = max(helper(nums, i + 1), nums[i] + helper(nums, i + 2));
+        return memo[i];
+    }
+};
