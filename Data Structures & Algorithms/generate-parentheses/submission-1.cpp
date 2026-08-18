6d11f9f288f8c39bf14cc879a77f2686c52cf1d2
Aditi Raj <aditiraj.novelist@gmail.com>
2026-08-18 10:29:33 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> res;
+    vector<string> generateParenthesis(int n) {
+        string s = "";
+        int open = n, close = n;
+        backtrack(open, close, s);
+        return res;
+    }
+
+    void backtrack(int& open, int& close, string& s)
+    {
+        if(open == 0 && close == 0)
+        {
+            res.push_back(s);
+            return;
+        }
+
+        if(open)
+        {
+            s += "(";
+            open--;
+            backtrack(open, close, s);
+            s.pop_back();
+            open++;
+        }
+
+        if(close && open < close)
+        {
+            s += ")";
+            close--;
+            backtrack(open, close, s);
+            s.pop_back();
+            close++;
+        }
+    }
+};
