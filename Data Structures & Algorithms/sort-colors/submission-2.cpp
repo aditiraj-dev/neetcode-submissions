9d7c4367581d0313a46b50915d57aa99e75fc7b0
Aditi Raj <aditiraj.novelist@gmail.com>
2026-08-19 10:53:24 +0530

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, l = 0, r = nums.size() - 1;
+
+        while(i <= r)
+        {
+            if(nums[i] == 0)
+            {
+                swap(nums[l], nums[i]);
+                l++;
+                i++;
+            }
+            else if(nums[i] == 2)
+            {
+                swap(nums[r], nums[i]);
+                r--;
+            }
+            else i++;
+        }
+    }
+};
